887dec7c95bf68c98a9e82b7aca48a0d61e80583
liran meir <liran.m@lawgeex.com>
2021-07-09 13:11:40 +0300

    hooking up react query

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,25 @@
+
+import { useQueryClient } from 'react-query'
+import { useDeletePostMutation, usePostsQuery } from '../generated'
+
 const Posts = ()=>{
-    return <></>
+    const queryClient = useQueryClient()
+    const { data, isLoading } = usePostsQuery()
+    const { mutate } = useDeletePostMutation({
+        onSuccess: () => queryClient.invalidateQueries('Posts'),
+    })
+    return <>
+    {isLoading && <p>Loading ...</p>}
+    {data &&
+        data.posts?.map(post => (
+            <div key={post?.id}>
+                <p>{post?.title}</p>
+                <button onClick={() => post && mutate({ id: post.id })}>
+                            Delete
+                        </button>
+                <hr />
+            </div>
+        ))}
+</>
 }
 export default Posts
